f5cc12e963b8a1ad6c121c57a00ec9794e08b2c4
aboutheo <alexandre.boutheon@ansys.com>
2024-01-05 10:56:55 +0100

    Added new functions and example file

---
+++ examples/001_load_write_wav_files.py
@@ -0,0 +1,75 @@
+"""
+.. _load_write_wav_iles_example:
+
+Load / Write Wav Files
+--------------------------
+
+This example shows how to load and write wav files, access the corresponding data and display it using numpy.
+
+"""
+# %%
+# Set up analysis
+# ~~~~~~~~~~~~~~~
+# Setting up the analysis consists of loading Ansys libraries, connecting to the
+# DPF server, and retrieving the example files.
+#
+# Load Ansys libraries.
+
+from ansys.pydpf_sound.library.sound_helpers import write_wav_signal, load_wav_signal
+import ansys.dpf.core as dpf
+import matplotlib.pyplot as plt
+import sys
+
+# Start a DPF server and copy the example files into the current working directory.
+dpf.server_context.set_default_server_context(dpf.AvailableServerContexts.premium)
+print("Server Context successfully created")
+
+# Change this path according to your needs
+path_to_dpf_server = r"C:\Program Files\ANSYS Inc\v242"
+s = dpf.start_local_server(ansys_path=path_to_dpf_server)
+
+# %%
+# Load DPF Sound plugin Actually loading the DPF Sound plugin
+try:
+    # Make sure the path below is correct
+    dpf.load_library(
+        r"C:\Program Files\ANSYS Inc\v242\Acoustics\SAS\ads\dpf_sound.dll",
+        "dpf_sound",
+    )
+    print("DPF Sound successfully loaded")
+
+except Exception as e:
+    # If we didn't manage to load the DLL, we end up here
+    print(e.args)
+    sys.exit("Error while loading dpf_sound.dll ! Aborting.")
+
+# %%
+# Load a wav signal usging load_wav_signal, it will be returned as a [DPF Field Container](https://dpf.docs.pyansys.com/version/stable/api/ansys.dpf.core.operators.utility.fields_container.html)
+
+# Modify the input path according to your needs
+fc_signal = load_wav_signal(r"C:\example_files\flute.wav")
+
+# %%
+# Create a modified version of the signal and plot the signals
+fc_signal_modified = dpf.FieldsContainer.deep_copy(fc_signal)
+fc_signal_modified[0].data = fc_signal[0].data * 0.2
+
+# Obtaining the [Time Frequency support](https://dpf.docs.pyansys.com/version/stable/api/ansys.dpf.core.time_freq_support.html) that contains the associated times of the signal
+time_support = fc_signal[0].time_freq_support.time_frequencies.data
+
+plt.plot(time_support, fc_signal[0].data, label="Original Signal")
+plt.plot(time_support, fc_signal_modified[0].data, label="Modified Signal")
+plt.title("My signals")
+plt.legend()
+plt.xlabel("s")
+plt.ylabel("Pa")
+plt.show()
+
+# %%
+# Write the modified signal in memory using write_wav_signal
+
+write_wav_signal(
+    r"C:\example_files\flute_modified.wav", fc_signal_modified, "int16"
+)
+
+print("End of script reached")
---
+++ src/ansys/pydpf_sound/library/sound_helpers.py
@@ -0,0 +1,66 @@
+"""Sound helpers."""
+__all__ = ("load_wav_signal", "write_wav_signal")
+
+from ansys.dpf.core import DataSources, Operator, fields_container
+
+
+def load_wav_signal(
+    input_path: str = "",
+) -> fields_container:
+    """Load a wav file from its input path.
+
+    Load a .wav file stored in memory using DPF Sound capabilities
+
+    Parameters
+    ----------
+    input_path:
+        The input path of the .wav file.
+    """
+
+    op_load_wav = Operator("load_wav_sas")  # For loading WAVs
+
+    # Loading a WAV file
+    data_source_in = DataSources()
+
+    # Creating input path
+    data_source_in.add_file_path(input_path, ".wav")
+
+    # Loading wav file and storing it into a container
+    op_load_wav.connect(0, data_source_in)
+
+    # Obtaining the output as a fieldsContainer
+    fc_signal = op_load_wav.get_output(0, "fields_container")
+    return fc_signal
+
+
+def write_wav_signal(
+    output_path: str = "",
+    data: fields_container = None,
+    bit_depth: str = "int16",
+) -> None:
+    """Write a wav file with its output path and its bit depth.
+
+    Load a .wav file stored in memory using DPF Sound capabilities
+
+    Parameters
+    ----------
+    output_path:
+        Path of the wav file to save.
+    data:
+        Signal to save: fields_container with each channel as a field.
+    bit_depth:
+        Bit depth. Supported values are: 'float32', 'int32', 'int16', 'int8'. This means that the samples will be respectively coded into the wav file using 32 bits (32-bit IEEE Float), 32 bits (int), 16 bits (int) or 8 bits (int).
+    """
+
+    op_write_wav = Operator("write_wav_sas")  # For writing WAV
+
+    data_source_out = DataSources()
+    data_source_out.add_file_path(output_path, ".wav")
+
+    op_write_wav.connect(0, data)
+    op_write_wav.connect(1, data_source_out)
+    op_write_wav.connect(2, bit_depth)
+
+    op_write_wav.run()
+
+    return
