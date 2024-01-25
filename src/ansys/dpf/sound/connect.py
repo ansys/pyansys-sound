037fe2f955d421109d6a5a29a907cca3e7d37fb1
aboutheo <alexandre.boutheon@ansys.com>
2024-01-25 11:28:18 +0100

    Fix : Modified dummy test

--- src/ansys/dpf/sound/connect.py
+++ src/ansys/dpf/sound/connect.py
@@ -1,9 +1,12 @@
 """Test if DPF Sound is available."""
 
+import ansys.dpf.core as dpf
 from ansys.dpf.core import Operator
 
 
 def validate_dpf_sound() -> None:
     """Validate that DPF Sound is available."""
+    dpf.start_local_server(ansys_path="C:\\ansys\\dpf\\server_2024_2_pre0\\")
+    dpf.load_library("C:\\ansys\\dpf\\server_2024_2_pre0\\Acoustics\\SAS\\ads\\dpf_sound.dll")
     Operator("load_wav_sas")
     print("DPF Sound is available and running.")
