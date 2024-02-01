e75364754c16561f32f32a16ec8a0af66e513e8b
aboutheo <alexandre.boutheon@ansys.com>
2024-02-01 10:04:59 +0100

    Test: changed port for simple test

--- src/ansys/dpf/sound/connect.py
+++ src/ansys/dpf/sound/connect.py
@@ -4,13 +4,13 @@ import os
 
 from ansys.dpf.core import Operator, connect_to_server, load_library
 
-DEFAULT_PORT: int = int(os.environ.get("ANSRV_DPF_SOUND_PORT", 678))
+DEFAULT_PORT: int = int(os.environ.get("ANSRV_DPF_SOUND_PORT", 6780))
 
 
 def validate_dpf_sound(port=None) -> None:
     """Validate that DPF Sound is available."""
     port = port if port is not None else DEFAULT_PORT
     server = connect_to_server(port=port)
-    load_library("Acoustics\\SAS\\ads\\dpf_sound.dll", server=server)
-    Operator("load_wav_sas", server=server)
+    load_library("dpf_sound.dll", "dpf_sound")
+    Operator("load_wav_sas")
     print("DPF Sound is available and running.")
