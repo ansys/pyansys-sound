759d370d345d1ef750c57144b6d5ade35807fa6e
Camille <78221213+clatapie@users.noreply.github.com>
2024-02-05 14:34:20 +0100

    fix: pdf build

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -145,6 +145,22 @@ source_suffix = ".rst"
 master_doc = "index"
 
 
+# -- Options for LaTeX output ------------------------------------------------
+latex_elements = {}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title,
+#  author, documentclass [howto, manual, or own class]).
+latex_documents = [
+    (
+        master_doc,
+        f"{project}-Documentation-{__version__}.tex",
+        f"{project} Documentation",
+        author,
+        "manual",
+    ),
+]
+
 from sphinx.builders.latex import LaTeXBuilder
 
 LaTeXBuilder.supported_image_types = ["image/png", "image/pdf", "image/svg+xml"]
