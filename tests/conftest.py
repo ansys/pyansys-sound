5e16f4346b82b68fc7da18623aae429462376310
aboutheo <alexandre.boutheon@ansys.com>
2024-02-05 17:29:41 +0100

    testing: implementing test for load_wav_file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,28 @@
+import os
+
+from ansys.dpf.core import connect_to_server, load_library
 import pytest
 
+CONTAINER_SERVER_PORT = 6780
+STR_DPF_SOUND = "dpf_sound.dll"
+STR_DPF_SOUND_DLL = "dpf_sound.dll"
 
-@pytest.fixture()
-def fixture():
-    return "This is a fixture"
+
+def pytest_configure():
+    pytest.data_path_flute_in_container = "C:\\data\\flute.wav"
+
+
+@pytest.fixture(scope="session")
+def dpf_sound_test_server():
+    port_in_env = os.environ.get("ANSRV_DPF_SOUND_PORT")
+    if port_in_env is not None:
+        port = int(port_in_env)
+    else:
+        port = CONTAINER_SERVER_PORT
+
+    # Connecting to server
+    server = connect_to_server(port=port)
+
+    # Loading DPF Sound
+    load_library(STR_DPF_SOUND_DLL, STR_DPF_SOUND)
+    yield server
--- tests/test_sound_helpers.py
+++ tests/test_sound_helpers.py
@@ -1,2 +1,18 @@
-def test_load_wav_file(fixture):
-    assert 1 == 1
+import pytest
+
+from ansys.dpf.sound.sound_helpers import load_wav_signal
+
+
+def test_load_wav_file(dpf_sound_test_server):
+    # Loading a wav signal using load_wav_signal
+    fc = load_wav_signal(pytest.data_path_flute_in_container)
+
+    # Extracting data
+    data = fc[0].data
+
+    # Checking data size and some random samples
+    assert len(data) == 156048
+    assert data[10] == 0.0
+    assert data[1000] == 6.103515625e-05
+    assert data[10000] == 0.0308837890625
+    assert data[136047] == -0.084686279296875
