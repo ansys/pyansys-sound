5095dc6c32bcad6158e3218311314df403b41bb5
aboutheo <alexandre.boutheon@ansys.com>
2024-02-06 10:50:24 +0100

    testing : Adding test badges + small typo fix

--- tests/test_example_helpers.py
+++ tests/test_example_helpers.py
@@ -3,4 +3,4 @@ from ansys.dpf.sound.examples_helpers import get_absolute_path_for_flute_wav
 
 def test_data_path_flute_wav():
     p = get_absolute_path_for_flute_wav()
-    assert p == "C\\data\\flute.wav"
+    assert p == "C:\\data\\flute.wav"
