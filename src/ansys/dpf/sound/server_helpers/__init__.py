8e45fa578f7106f78cbc317bd76f219def86deba
aboutheo <alexandre.boutheon@ansys.com>
2024-02-06 09:09:39 +0100

    fix : adding test for validating connection

--- src/ansys/dpf/sound/server_helpers/__init__.py
+++ src/ansys/dpf/sound/server_helpers/__init__.py
@@ -4,6 +4,6 @@ Helper functions for managing the DPF server, in particular for loading
 the DPF Sound plugin.
 """
 
-from ._connect_to_or_start_server import connect_to_or_start_server
+from ._connect_to_or_start_server import connect_to_or_start_server, validate_dpf_sound
 
-__all__ = ("connect_to_or_start_server",)
+__all__ = ("connect_to_or_start_server", "validate_dpf_sound")
---
+++ src/ansys/dpf/sound/server_helpers/validate_connection.py
@@ -0,0 +1,16 @@
+"""Test if DPF Sound is available."""
+
+import os
+
+from ansys.dpf.core import Operator, connect_to_server, load_library
+
+DEFAULT_PORT: int = int(os.environ.get("ANSRV_DPF_SOUND_PORT", 6780))
+
+
+def validate_dpf_sound(port=None) -> None:
+    """Validate that DPF Sound is available."""
+    port = port if port is not None else DEFAULT_PORT
+    connect_to_server(port=port)
+    load_library("dpf_sound.dll", "dpf_sound")
+    Operator("load_wav_sas")
+    print("DPF Sound is available and running.")
