6d36baf59532e2ef0bb033892f9b51ec2225b978
aboutheo <alexandre.boutheon@ansys.com>
2024-02-06 09:34:00 +0100

    fix : better renaming

--- src/ansys/dpf/sound/server_helpers/_validate_dpf_sound_connection.py
+++ src/ansys/dpf/sound/server_helpers/_validate_dpf_sound_connection.py
@@ -7,7 +7,7 @@ from ansys.dpf.core import Operator, connect_to_server, load_library
 DEFAULT_PORT: int = int(os.environ.get("ANSRV_DPF_SOUND_PORT", 6780))
 
 
-def validate_dpf_sound(port=None) -> None:
+def validate_dpf_sound_connection(port=None) -> None:
     """Validate that DPF Sound is available."""
     port = port if port is not None else DEFAULT_PORT
     connect_to_server(port=port)
