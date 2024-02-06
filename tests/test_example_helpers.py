e5a419ef17b4e3042c165f26610806eb8484d7a1
aboutheo <alexandre.boutheon@ansys.com>
2024-02-06 10:39:05 +0100

    testing : updating code coverage

---
+++ tests/test_example_helpers.py
@@ -0,0 +1,6 @@
+from ansys.dpf.sound.examples_helpers import get_absolute_path_for_flute_wav
+
+
+def test_data_path_flute_wav():
+    p = get_absolute_path_for_flute_wav()
+    assert p == "C\\data\\flute.wav"
