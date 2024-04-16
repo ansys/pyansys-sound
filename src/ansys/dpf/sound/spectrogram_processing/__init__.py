771c018056f8379315bf9fd522517a4eec1e8516
aboutheo <alexandre.boutheon@ansys.com>
2024-04-16 15:15:51 +0200

    dev : created mother class for spectrogram processing

---
+++ src/ansys/dpf/sound/spectrogram_processing/__init__.py
@@ -0,0 +1,8 @@
+"""Spectrogram processing classes.
+
+Helper functions related to spectrogram processing.
+"""
+
+from ._spectrogram_processing_parent import SpectrogramProcessingParent  # isort:skip
+
+__all__ = "SpectrogramProcessingParent"
