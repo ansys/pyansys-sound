4057dd79b7e153ecace02067dbad6dc959439553
milanredon <milan.redon@ansys.com>
2024-05-27 17:31:52 +0200

    Improved existing example

--- examples/001_load_write_wav_files.py
+++ examples/001_load_write_wav_files.py
@@ -1,28 +1,31 @@
 # Copyright (C) 2023 - 2024 ANSYS, Inc. and/or its affiliates.
 
 """
-.. _load_write_wav_files_example:
+.. _load_resample_amplify_write_wav_files_example:
 
-Load / Write Wav Files
+Load / Write Wav Files, resample and apply gains
 ----------------------
 
-This example shows how to load and write wav files.
+This example shows how to load a Wav file, modify its samply frequency, amplify it and write the resulting Wav file in memory.
 It also shows how to access the corresponding data and display it using numpy.
 
 """
-# %%
-# Set up analysis
+# %% 
+# Set up analysis 
 # ~~~~~~~~~~~~~~~
-# Setting up the analysis consists of loading Ansys libraries, connecting to the
+# Setting up the analysis consists of loading Ansys libraries, connecting to the 
 # DPF server, and retrieving the example files.
 #
-# Load Ansys libraries.
+# Load Ansys & other libraries.
 
 import ansys.dpf.core as dpf
 
 from ansys.dpf.sound.examples_helpers import get_absolute_path_for_flute_wav
 from ansys.dpf.sound.server_helpers import connect_to_or_start_server
-from ansys.dpf.sound.signal_utilities import LoadWav, WriteWav
+from ansys.dpf.sound.signal_utilities import LoadWav, Resample, ApplyGain, WriteWav
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Connect to remote or start a local server
 connect_to_or_start_server(ansys_path=r'C:\Program Files\ANSYS Inc\v242\ansys_dpf_server_win_v2024.2.pre1\ansys\dpf\server_2024_2_pre1')
@@ -37,14 +40,56 @@ path_flute_wav = get_absolute_path_for_flute_wav()
 # Load the wav file.
 wav_loader = LoadWav(path_flute_wav)
 wav_loader.process()
-fc_signal = wav_loader.get_output()
-
-print("TEST")
+fc_signal_original = wav_loader.get_output()
 
 # %%
-# Plot the loaded signal.
-wav_loader.plot()
+# Change the sampling frequency of the loaded signal.
+resampler = Resample(fc_signal_original, new_sampling_frequency = 20000)
+resampler.process()
+fc_signal_resampled = resampler.get_output()
 
+t1 = fc_signal_original[0].time_freq_support.time_frequencies.data 
+print (f"The sampling frequency of the original signal is {1.0/(t1[1] - t1[0]):.1f} Hz" )
+
+# %%
+# Change the gain of the resampled signal.
+gain_applier= ApplyGain(fc_signal_resampled, gain = 10, gain_in_db = True)
+gain_applier.process()
+fc_signal_modified = gain_applier.get_output()
+
+t2 = fc_signal_modified[0].time_freq_support.time_frequencies.data 
+print (f"The new sampling frequency of the signal is {1.0/(t2[1] - t2[0]):.1f} Hz" )
+
+# %%
+# Plot the original and the modified signals.
+data_original = wav_loader.get_output_as_nparray()
+data_modified = gain_applier.get_output_as_nparray()
+
+fig, axs = plt.subplots(2)
+fig.suptitle('Signals')
+
+axs[0].plot(t1 , data_original , color='g', label='original signal')
+axs[0].set_ylabel('Pa')
+axs[0].legend(loc="upper right")
+axs[0].set_ylim([-3, 3])
+
+axs[1].plot(t2 , data_modified , color='r', label='modified signal')
+axs[1].set_xlabel('s')
+axs[1].set_ylabel('Pa')
+axs[1].legend(loc="upper right")
+axs[1].set_ylim([-3, 3])
+
+plt.show()
+
+
+
+# %% Write the modified signal in memory using WriteWav class
+output_path = path_flute_wav[:-4] + "_modified.wav" #[-4] is to remove the ".wav"
+wav_writer = WriteWav(path_to_write=output_path, signal=fc_signal_modified, bit_depth="int16")
+wav_writer.process()
+print("End of script reached")
+
+'''
 # %%
 # Create a modified version of the signal.
 #
@@ -55,6 +100,4 @@ fc_signal_modified = dpf.FieldsContainer.deep_copy(fc_signal)
 fc_signal_modified[0].data = fc_signal[0].data * 0.2
 output_path = path_flute_wav[:-4] + "_modified.wav"
 
-wav_writer = WriteWav(path_to_write=output_path, signal=fc_signal_modified, bit_depth="int16")
-wav_writer.process()
-print("End of script reached")
+'''
