e91ec7a0fd15d3336c1eb32c0d9b8ac5e8b41da4
aboutheo <alexandre.boutheon@ansys.com>
2024-05-30 15:14:13 +0200

    added example files

--- src/ansys/dpf/sound/examples_helpers/__init__.py
+++ src/ansys/dpf/sound/examples_helpers/__init__.py
@@ -13,6 +13,8 @@ from ._get_example_files import (
     get_absolute_path_for_rough_tone_wav,
     get_absolute_path_for_sharp_noise_wav,
     get_absolute_path_for_sharper_noise_wav,
+    get_absolute_path_for_xtract_demo_signal_1_wav,
+    get_absolute_path_for_xtract_demo_signal_2_wav,
 )
 
 __all__ = (
@@ -25,4 +27,6 @@ __all__ = (
     "get_absolute_path_for_rough_tone_wav",
     "get_absolute_path_for_fluctuating_noise_wav",
     "get_absolute_path_for_fluctuating_tone_wav",
+    "get_absolute_path_for_xtract_demo_signal_1_wav",
+    "get_absolute_path_for_xtract_demo_signal_2_wav",
 )
