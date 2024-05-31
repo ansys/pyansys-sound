b835f60e95dbb5596301712d4ad80d0a783cace2
Antoine Minard <antoine.minard@ansys.com>
2024-05-31 10:20:50 +0200

    Missing space char

--- examples/007_calculate_psychoacoustic_indicators.py
+++ examples/007_calculate_psychoacoustic_indicators.py
@@ -11,7 +11,7 @@ The following indicators are included:
 - Loudness of stationary sounds according to ISO 532-1;
 - Loudness of time-varying sounds according to ISO 532-1;
 - Sharpness according to Zwicker and Fastl, "Psychoacoustics: Facts and models", 1990;
-- Roughness according to Daniel and Weber, "Psychoacoustical Roughness:Implementation of an
+- Roughness according to Daniel and Weber, "Psychoacoustical Roughness: Implementation of an
   Optimized Model, 1997;
 - Fluctuation strength according to Sontacchi, "Entwicklung eines Modulkonzeptes für die
   psychoakustische Geräuschanalyse under MatLab Diplomarbeit", 1998.
