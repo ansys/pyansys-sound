654bdc684035ce575fdb5943657ee66358414f83
Stéphane Molla <124169343+anssmolla@users.noreply.github.com>
2024-05-31 22:14:30 +0200

    Example 4: deal with server startup and show how to use DPF license context (#23)
    Co-authored-by: aboutheo <alexandre.boutheon@ansys.com>
    Co-authored-by: Antoine Minard <165933065+ansaminard@users.noreply.github.com>

--- src/ansys/dpf/sound/server_helpers/_connect_to_or_start_server.py
+++ src/ansys/dpf/sound/server_helpers/_connect_to_or_start_server.py
@@ -3,11 +3,19 @@
 import os
 from typing import Any, Optional, Union
 
-from ansys.dpf.core import connect_to_server, load_library, start_local_server
+from ansys.dpf.core import (
+    LicenseContextManager,
+    connect_to_server,
+    load_library,
+    start_local_server,
+)
 
 
 def connect_to_or_start_server(
-    port: Optional[int] = None, ip: Optional[str] = None, ansys_path: Optional[str] = None
+    port: Optional[int] = None,
+    ip: Optional[str] = None,
+    ansys_path: Optional[str] = None,
+    use_license_context: Optional[bool] = False,
 ) -> Any:
     r"""Connect to or start a DPF server with the DPF Sound plugin loaded.
 
@@ -26,6 +34,12 @@ def connect_to_or_start_server(
     ansys_path :
         Root path for the Ansys installation. For example, ``C:\\Program Files\\ANSYS Inc\\v232``.
         This parameter is ignored if either the port or IP address is set.
+    use_license_context :
+        Option to check out DPF Sound license increment (avrxp_snd_level1) before using
+        PyAnsys Sound. It improves performance if you are doing multiple calls to DPF Sound
+        operators, as these require licensing. This option can also be used to force check out
+        before running a script when few DPF Sound license increments are available.
+        The license will be checked in when the server object is deleted.
 
     Returns
     -------
@@ -49,12 +63,10 @@ def connect_to_or_start_server(
         server = connect_to_server(
             **connect_kwargs,
         )
+        full_path_dll = "Acoustics\\SAS\\ads\\"
     else:  # pragma: no cover
-        server = start_local_server(
-            ansys_path=ansys_path,
-        )
-
-        full_path_dll = os.path.join(ansys_path, "Acoustics\\SAS\\ads\\")
+        server = start_local_server(ansys_path=ansys_path)
+        full_path_dll = os.path.join(server.ansys_path, "Acoustics\\SAS\\ads\\")
 
     required_version = "8.0"
     server.check_version(
@@ -63,5 +75,15 @@ def connect_to_or_start_server(
         f"(Ansys 2024 R2) or later. Your version is currently {server.version}.",
     )
 
-    load_library(full_path_dll + "dpf_sound.dll", "dpf_sound")
+    load_library(full_path_dll + "dpf_sound.dll", "dpf_sound", server=server)
+
+    # if required, check out the DPF Sound license once and for all for this session
+    lic_context = None
+    if use_license_context == True:
+        lic_context = LicenseContextManager(increment_name="avrxp_snd_level1", server=server)
+
+    # "attach" the license context to the server as a member so that they have the same
+    # life duration
+    server.license_context_manager = lic_context
+
     return server
