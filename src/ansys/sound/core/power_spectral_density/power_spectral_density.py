6bd5c95ac370355df93a8a809316455e4c3caa5a
cquesson <cyrille.quesson@ansys.com>
2024-06-28 11:20:12 +0200

    Power Spectral Density with his test suite available.

--- src/ansys/sound/core/power_spectral_density/power_spectral_density.py
+++ src/ansys/sound/core/power_spectral_density/power_spectral_density.py
@@ -1,3 +1,25 @@
+# Copyright (C) 2023 - 2024 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 """Power Spectral Density (PSD) module."""
 
 import warnings
@@ -8,10 +30,10 @@ import numpy as np
 from numpy import typing as npt
 
 from . import PowerSpectralDensityParent
-
 from .._pyansys_sound import PyAnsysSoundException, PyAnsysSoundWarning
 
 ID_POWER_SPECTRAL_DENSITY = "compute_power_spectral_density"
+
 
 class PowerSpectralDensity(PowerSpectralDensityParent):
     """Power Spectral Density (PSD) class.
@@ -19,7 +41,14 @@ class PowerSpectralDensity(PowerSpectralDensityParent):
     This class provides the Power Spectral Density (PSD) calculation for a given signal.
     """
 
-    def __init__(self, input_signal: Field, window_type: str = "HANN", window_length: int = 2048, fft_size: int = 2048, overlap: int = 1536):
+    def __init__(
+        self,
+        input_signal: Field,
+        window_type: str = "HANN",
+        window_length: int = 2048,
+        fft_size: int = 2048,
+        overlap: int = 1536,
+    ):
         """Init.
 
         Init the class.
@@ -40,10 +69,9 @@ class PowerSpectralDensity(PowerSpectralDensityParent):
 
     @property
     def input_signal(self) -> Field:
-        """Input signal.
-        """
+        """Input signal."""
         return self.__input_signal  # pragma: no cover
-    
+
     @input_signal.setter
     def input_signal(self, value: Field):
         """Set input signal."""
@@ -51,10 +79,9 @@ class PowerSpectralDensity(PowerSpectralDensityParent):
 
     @property
     def window_type(self) -> str:
-        """Window type.
-        """
+        """Window type."""
         return self.__window_type  # pragma: no cover
-    
+
     @window_type.setter
     def window_type(self, value: str):
         """Set window type."""
@@ -62,10 +89,9 @@ class PowerSpectralDensity(PowerSpectralDensityParent):
 
     @property
     def window_length(self) -> int:
-        """Window length.
-        """
+        """Window length."""
         return self.__window_length  # pragma: no cover
-    
+
     @window_length.setter
     def window_length(self, value: int):
         """Set window length."""
@@ -73,10 +99,9 @@ class PowerSpectralDensity(PowerSpectralDensityParent):
 
     @property
     def fft_size(self) -> int:
-        """FFT size.
-        """
+        """FFT size."""
         return self.__fft_size  # pragma: no cover
-    
+
     @fft_size.setter
     def fft_size(self, value: int):
         """Set FFT size."""
@@ -84,11 +109,72 @@ class PowerSpectralDensity(PowerSpectralDensityParent):
 
     @property
     def overlap(self) -> int:
-        """Overlap.
-        """
+        """Overlap."""
         return self.__overlap  # pragma: no cover
-    
+
     @overlap.setter
     def overlap(self, value: int):
         """Set overlap."""
         self.__overlap = value
+
+    def process(self):
+        """Process the Power Spectral Density (PSD) calculation.
+
+        This method processes the Power Spectral Density (PSD) calculation.
+        """
+        # Check input signal
+        if self.input_signal is None:
+            raise PyAnsysSoundException("Input signal is not set")
+
+        # Set operator inputs
+        self.__operator.connect(0, self.input_signal)
+        self.__operator.connect(1, self.window_type)
+        self.__operator.connect(2, self.window_length)
+        self.__operator.connect(3, self.fft_size)
+        self.__operator.connect(4, self.overlap)
+
+        # Run the operator
+        self.__operator.run()
+
+        # Get the output
+        self._output = self.__operator.get_output(0, "field")
+
+    def get_output(self) -> Field:
+        """Get the output."""
+        if self._output is None:
+            warnings.warn(PyAnsysSoundWarning("No output is available."))
+
+        return self._output
+
+    def get_output_as_nparray(self) -> npt.ArrayLike:
+        """Get the output as a numpy array.
+
+        Returns
+        -------
+        numpy.ndarray
+            The output as a numpy array.
+        """
+        l_output = self.get_output()
+
+        if l_output is None:
+            return (np.array([]), np.array([]))
+
+        l_psd = l_output.data
+        l_frequencies = l_output.time_freq_support.time_frequencies.data
+
+        return (np.array(l_psd), np.array(l_frequencies))
+
+    def plot(self):
+        """Plot the Power Spectral Density (PSD) calculation.
+
+        This method plots the Power Spectral Density (PSD) calculation.
+        """
+        # Get the output
+        psd_values, l_frequencies = self.get_output_as_nparray()
+
+        # Plot the PSD
+        plt.plot(l_frequencies, psd_values)
+        plt.title("Power Spectral Density (PSD)")
+        plt.xlabel("Frequency")
+        plt.ylabel("PSD")
+        plt.show()
--- tests/tests_power_spectral_density/test_power_spectral_density.py
+++ tests/tests_power_spectral_density/test_power_spectral_density.py
@@ -1,3 +1,25 @@
+# Copyright (C) 2023 - 2024 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from unittest.mock import patch
 
 from ansys.dpf.core import Field
@@ -5,8 +27,8 @@ import numpy as np
 import pytest
 
 from ansys.sound.core._pyansys_sound import PyAnsysSoundException, PyAnsysSoundWarning
-from ansys.sound.core.signal_utilities import LoadWav
 from ansys.sound.core.power_spectral_density import PowerSpectralDensity
+from ansys.sound.core.signal_utilities import LoadWav
 
 
 def test_power_spectral_density_instantiation(dpf_sound_test_server):
@@ -42,69 +64,108 @@ def test_power_spectral_density_setters(dpf_sound_test_server):
     assert psd.overlap == 512
 
 
+def test_warning(dpf_sound_test_server):
+    """Test PowerSpectralDensity warning."""
+    with pytest.warns(PyAnsysSoundWarning) as record:
+        psd = PowerSpectralDensity(Field())
+        psd.get_output()
+    assert record[0].message.args[0] == "No output is available."
+
+
+def test_exception(dpf_sound_test_server):
+    """Test PowerSpectralDensity exception."""
+    with pytest.raises(PyAnsysSoundException) as record:
+        psd = PowerSpectralDensity(Field())
+        psd.input_signal = None
+        psd.process()
+    assert record.value.args[0] == "Input signal is not set"
+
+
+def test_noprocess_get_output_as_nparray(dpf_sound_test_server):
+    """Test PowerSpectralDensity get_output_as_nparray without process."""
+    psd = PowerSpectralDensity(Field())
+    psd_values, psd_frequencies = psd.get_output_as_nparray()
+
+    assert type(psd_values) == np.ndarray
+    assert type(psd_frequencies) == np.ndarray
+
+    assert len(psd_values) == 0
+    assert len(psd_frequencies) == 0
+
+
 def test_power_spectral_density_noprocess(dpf_sound_test_server):
     """Test PowerSpectralDensity without process."""
     # Test without process
     psd = PowerSpectralDensity(Field())
-    with pytest.raises(PyAnsysSoundException):
-        psd.output
+    with pytest.warns(PyAnsysSoundWarning) as record:
+        psd.get_output()
+    assert record[0].message.args[0] == "No output is available."
 
 
 def test_power_spectral_density_process(dpf_sound_test_server):
-    """Test PowerSpectralDensity process."""
-    # Test process
-    psd = PowerSpectralDensity(Field())
-    with pytest.raises(PyAnsysSoundException):
-        psd.process()
-
     # Test process with input signal
-    input_signal = LoadWav(pytest.data_path_flute_in_container)
-    psd.input_signal = input_signal
+    op_load_wav = LoadWav(pytest.data_path_flute_in_container)
+    op_load_wav.process()
+    input_signal = op_load_wav.get_output()[0]
+
+    psd = PowerSpectralDensity(input_signal)
     psd.process()
-    assert psd.output is not None
+
+    assert psd.get_output() is not None
 
 
 def test_power_spectral_density_get_output(dpf_sound_test_server):
     """Test PowerSpectralDensity get_output."""
     # Test get_output
-    psd = PowerSpectralDensity(Field())
-    with pytest.raises(PyAnsysSoundException):
-        psd.get_output()
+    op_load_wav = LoadWav(pytest.data_path_flute_in_container)
+    op_load_wav.process()
+    input_signal = op_load_wav.get_output()[0]
 
-    # Test get_output with input signal
-    input_signal = LoadWav(pytest.data_path_flute_in_container)
-    psd.input_signal = input_signal
+    psd = PowerSpectralDensity(input_signal)
     psd.process()
+
     output = psd.get_output()
     assert output is not None
+
+    # Check psd values
+    assert output.data[0] == pytest.approx(1.27088645e-10)
+    assert output.data[11] == pytest.approx(2.05275264e-05)
+    assert output.data[12] == pytest.approx(0.000141005585)
+    assert output.data[24] == pytest.approx(0.000813542807)
+    assert output.data[26] == pytest.approx(1.11639283e-05)
+    assert output.data[36] == pytest.approx(0.000375645177)
+    assert output.data[37] == pytest.approx(0.000426257000)
+    assert output.data[209] == pytest.approx(5.73722625e-09)
+
+    # Check frequencies
+    assert output.time_freq_support.time_frequencies.data[0] == pytest.approx(0.0)
+    assert output.time_freq_support.time_frequencies.data[1] == pytest.approx(21.533203125000000)
+    assert output.time_freq_support.time_frequencies.data[1023] == pytest.approx(22028.466796875000)
+    assert output.time_freq_support.time_frequencies.data[1024] == pytest.approx(22050.000000000000)
 
 
 def test_power_spectral_density_get_output_as_nparray(dpf_sound_test_server):
     """Test PowerSpectralDensity get_output_as_nparray."""
-    # Test get_output_as_nparray
-    psd = PowerSpectralDensity(Field())
-    with pytest.raises(PyAnsysSoundException):
-        psd.get_output_as_nparray()
+    op_load_wav = LoadWav(pytest.data_path_flute_in_container)
+    op_load_wav.process()
+    input_signal = op_load_wav.get_output()[0]
 
-    # Test get_output_as_nparray with input signal
-    input_signal = LoadWav(pytest.data_path_flute_in_container)
-    psd.input_signal = input_signal
+    psd = PowerSpectralDensity(input_signal)
     psd.process()
-    output = psd.get_output_as_nparray()
-    assert output is not None
+
+    psd_values, psd_frequencies = psd.get_output_as_nparray()
+
+    assert psd_values[0] == pytest.approx(1.27088645e-10)
+    assert psd_frequencies[1] == pytest.approx(21.533203125000000)
 
 
 @patch("matplotlib.pyplot.show")
 def test_power_spectral_density_plot(mock_show, dpf_sound_test_server):
     """Test PowerSpectralDensity plot."""
-    # Test plot
-    psd = PowerSpectralDensity(Field())
-    with pytest.raises(PyAnsysSoundException):
-        psd.plot()
+    op_load_wav = LoadWav(pytest.data_path_flute_in_container)
+    op_load_wav.process()
+    input_signal = op_load_wav.get_output()[0]
 
-    # Test plot with input signal
-    input_signal = LoadWav(pytest.data_path_flute_in_container)
-    psd.input_signal = input_signal
+    psd = PowerSpectralDensity(input_signal)
     psd.process()
     psd.plot()
-    mock_show.assert_called()
