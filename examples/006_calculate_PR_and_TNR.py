8909298a827090c961b615a04784de0ea3081515
Cyrille Quesson <164036621+anscquesson@users.noreply.github.com>
2024-07-10 09:24:29 +0200

    Update examples/006_calculate_PR_and_TNR.py
    Co-authored-by: Stéphane Molla <124169343+anssmolla@users.noreply.github.com>

--- examples/006_calculate_PR_and_TNR.py
+++ examples/006_calculate_PR_and_TNR.py
@@ -151,7 +151,7 @@ wav_loader.process()
 flute_signal = wav_loader.get_output()[0]
 
 # %%
-# Create a PowerSpectralDensity (psd) object, set its signal, and compute the psd.
+# Create a PowerSpectralDensity object, set its input signal and parameters, then compute the PSD.
 psd_object = PowerSpectralDensity(
     flute_signal, fft_size=8192, window_type="HANN", window_length=8192, overlap=0.8
 )
