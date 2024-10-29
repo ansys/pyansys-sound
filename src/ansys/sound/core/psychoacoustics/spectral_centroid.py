6576a6c97b621a78f480d4d76996b2d14238a83c
Helene Lachambre <helene.lachambre@ansys.com>
2024-10-29 15:03:33 +0100

    feat: new indicator SpectralCentroid, with its associated tests

---
+++ src/ansys/sound/core/psychoacoustics/spectral_centroid.py
@@ -0,0 +1,142 @@
+# Copyright (C) 2023 - 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""Computes the spectral centroid of the signal."""
+import warnings
+
+from ansys.dpf.core import Field, Operator
+import numpy as np
+from numpy import typing as npt
+
+from . import PsychoacousticsParent
+from .._pyansys_sound import PyAnsysSoundException, PyAnsysSoundWarning
+
+
+class SpectralCentroid(PsychoacousticsParent):
+    """Computes the spectral centroid of a signal."""
+
+    def __init__(self, signal: Field = None):
+        """Create a ``SpectralCentroid`` object.
+
+        Parameters
+        ----------
+        signal: Field
+            Signal in Pa to compute spectral centroid on as a DPF field.
+        """
+        super().__init__()
+        self.signal = signal
+        self.__operator = Operator("compute_spectral_centroid")
+
+    @property
+    def signal(self):
+        """Signal."""
+        return self.__signal  # pragma: no cover
+
+    @signal.setter
+    def signal(self, signal: Field):
+        """Set the signal.
+
+        Parameters
+        -------
+        signal: Field
+            Signal in Pa to compute spectral centroid on as a DPF field.
+        """
+        self.__signal = signal
+
+    @signal.getter
+    def signal(self) -> Field:
+        """Signal.
+
+        Returns
+        -------
+        Field
+            Signal in Pa as a DPF field.
+        """
+        return self.__signal
+
+    def process(self):
+        """Compute the spectral centroid."""
+        if self.signal is None:
+            raise PyAnsysSoundException(
+                "No signal found for spectral centroid computation. Use 'SpectralCentroid.signal'."
+            )
+
+        self.__operator.connect(0, self.signal)
+
+        # Run the operator
+        self.__operator.run()
+
+        # Stores output
+        self._output = self.__operator.get_output(0, "double")
+
+    def get_output(self) -> float:
+        """Get the spectral centroid.
+
+        Returns
+        -------
+        float
+            Spectral centroid in Hz.
+        """
+        if self._output == None:
+            warnings.warn(
+                PyAnsysSoundWarning(
+                    "Output is not processed yet. \
+                        Use the 'SpectralCentroid.process()' method."
+                )
+            )
+
+        return self._output
+
+    def get_output_as_nparray(self) -> npt.ArrayLike:
+        """Get the spectral centroid as a NumPy array.
+
+        Returns
+        -------
+        numpy.ndarray:
+            Array of spectral centroid values in Hz.
+        """
+        return np.array([self._output])
+
+    def get_spectral_centroid(self) -> float:
+        """Get the spectral centroid.
+
+        Returns
+        -------
+        float
+            Spectral centroid in Hz.
+        """
+        return self._output
+
+    def __str__(self):
+        """Create the string for print() call.
+
+        Returns
+        -------
+        str
+            Formatted info about self.
+        """
+        str = __class__.__name__ + "\n"
+        str += "Data\n"
+        str += "\t Signal name: " + self.signal.name + "\n"
+        str += f"\t Spectral centroid: {self.get_spectral_centroid()} Hz\n"
+
+        return str
---
+++ tests/tests_psychoacoustics/test_psychoacoustics_spectral_centroid.py
@@ -0,0 +1,155 @@
+# Copyright (C) 2023 - 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from ansys.dpf.core import Field
+import numpy as np
+import pytest
+
+from ansys.sound.core._pyansys_sound import PyAnsysSoundException, PyAnsysSoundWarning
+from ansys.sound.core.psychoacoustics import SpectralCentroid
+from ansys.sound.core.signal_utilities import LoadWav
+
+EXP_SPECTRAL_CENTROID = 816.00695
+
+
+def test_spectral_centroid_instantiation(dpf_sound_test_server):
+    spectral_centroid_computer = SpectralCentroid()
+    assert spectral_centroid_computer != None
+
+
+def test_spectral_centroid_process(dpf_sound_test_server):
+    spectral_centroid_computer = SpectralCentroid()
+
+    # No signal -> error
+    with pytest.raises(
+        PyAnsysSoundException,
+        match="No signal found for spectral centroid computation. Use 'SpectralCentroid.signal'.",
+    ):
+        spectral_centroid_computer.process()
+
+    # Get a signal
+    wav_loader = LoadWav(pytest.data_path_flute_nonUnitaryCalib_in_container)
+    wav_loader.process()
+    fc = wav_loader.get_output()
+
+    # Set signal as field
+    spectral_centroid_computer.signal = fc[0]
+
+    # Compute: no error
+    spectral_centroid_computer.process()
+
+
+def test_spectral_centroid_get_output(dpf_sound_test_server):
+    spectral_centroid_computer = SpectralCentroid()
+
+    # Get a signal
+    wav_loader = LoadWav(pytest.data_path_flute_nonUnitaryCalib_in_container)
+    wav_loader.process()
+    fc = wav_loader.get_output()
+
+    # Set signal
+    spectral_centroid_computer.signal = fc[0]
+
+    # Spectral centroid not calculated yet -> warning
+    with pytest.warns(
+        PyAnsysSoundWarning,
+        match="Output is not processed yet. \
+                        Use the 'SpectralCentroid.process\\(\\)' method.",
+    ):
+        spectral_centroid_computer.get_output()
+
+    # Compute
+    spectral_centroid_computer.process()
+
+    spectral_centroid = spectral_centroid_computer.get_output()
+    assert spectral_centroid != None
+    assert type(spectral_centroid) == float
+    assert spectral_centroid == pytest.approx(EXP_SPECTRAL_CENTROID)
+
+
+def test_spectral_centroid_get_spectral_centroid(dpf_sound_test_server):
+    spectral_centroid_computer = SpectralCentroid()
+    # Get a signal
+    wav_loader = LoadWav(pytest.data_path_flute_nonUnitaryCalib_in_container)
+    wav_loader.process()
+    fc = wav_loader.get_output()
+
+    # Set signal
+    spectral_centroid_computer.signal = fc[0]
+
+    # Compute
+    spectral_centroid_computer.process()
+
+    spectral_centroid = spectral_centroid_computer.get_spectral_centroid()
+    assert spectral_centroid != None
+    assert type(spectral_centroid) == float
+    assert spectral_centroid == pytest.approx(EXP_SPECTRAL_CENTROID)
+
+
+def test_spectral_centroid_get_output_as_nparray(dpf_sound_test_server):
+    spectral_centroid_computer = SpectralCentroid()
+    # Get a signal
+    wav_loader = LoadWav(pytest.data_path_flute_nonUnitaryCalib_in_container)
+    wav_loader.process()
+    fc = wav_loader.get_output()
+
+    # Set signal
+    spectral_centroid_computer.signal = fc[0]
+
+    # Compute
+    spectral_centroid_computer.process()
+
+    spectral_centroid = spectral_centroid_computer.get_output_as_nparray()
+    assert spectral_centroid != None
+    assert type(spectral_centroid) == np.ndarray
+    assert spectral_centroid[0] == pytest.approx(EXP_SPECTRAL_CENTROID)
+
+
+def test_spectral_centroid_set_get_signal(dpf_sound_test_server):
+    spectral_centroid_computer = SpectralCentroid()
+    field = Field()
+    field.data = 42 * np.ones(3)
+    spectral_centroid_computer.signal = field
+    field_from_get = spectral_centroid_computer.signal
+
+    assert field_from_get.data[0, 2] == 42
+
+
+def test_spectral_centroid_print(dpf_sound_test_server):
+    spectral_centroid_computer = SpectralCentroid()
+    # Get a signal
+    wav_loader = LoadWav(pytest.data_path_flute_nonUnitaryCalib_in_container)
+    wav_loader.process()
+    fc = wav_loader.get_output()
+
+    # Set signal
+    spectral_centroid_computer.signal = fc[0]
+
+    # Compute
+    spectral_centroid_computer.process()
+
+    # Check __str__
+    str = spectral_centroid_computer.__str__()
+    assert (
+        str
+        == "SpectralCentroid\nData\n\t Signal name: flute\n\t Spectral centroid: 816.0070190429688 Hz\n"  # noqa: E501
+    )
