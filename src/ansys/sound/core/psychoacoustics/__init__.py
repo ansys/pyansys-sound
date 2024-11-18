c817c003a8dfa8920354caac8e165c3c7f5e5d21
Antoine Minard <antoine.minard@ansys.com>
2024-11-18 18:27:25 +0100

    ommitted __init__.py

--- src/ansys/sound/core/psychoacoustics/__init__.py
+++ src/ansys/sound/core/psychoacoustics/__init__.py
@@ -32,8 +32,8 @@ from .loudness_iso_532_1_time_varying import LoudnessISO532_1_TimeVarying
 from .prominence_ratio import ProminenceRatio
 from .roughness import Roughness
 from .sharpness import Sharpness
-from .tone_to_noise_ratio import ToneToNoiseRatio
 from .tonality_din_45681 import TonalityDIN45681
+from .tone_to_noise_ratio import ToneToNoiseRatio
 
 __all__ = (
     "PsychoacousticsParent",
