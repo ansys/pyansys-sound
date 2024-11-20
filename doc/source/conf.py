b2affb0fb2b8a7a63b7db6e48eb7d7383e54ba71
Camille <78221213+clatapie@users.noreply.github.com>
2024-11-20 16:14:27 +0100

    fix: removing ``autoclass_content`` configuration

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -106,9 +106,6 @@ intersphinx_mapping = {
 numpydoc_show_class_members = False
 numpydoc_xref_param_type = True
 
-# autodoc configuration
-autoclass_content = "both"
-
 # Consider enabling numpydoc validation. See:
 # https://numpydoc.readthedocs.io/en/latest/validation.html#
 numpydoc_validate = True
