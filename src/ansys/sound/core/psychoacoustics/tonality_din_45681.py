bbc5471a5e39343406dcfaeea88dbd927d93ffb5
Antoine Minard <antoine.minard@ansys.com>
2024-11-26 16:30:01 +0100

    typo

--- src/ansys/sound/core/psychoacoustics/tonality_din_45681.py
+++ src/ansys/sound/core/psychoacoustics/tonality_din_45681.py
@@ -307,8 +307,8 @@ class TonalityDIN45681(PsychoacousticsParent):
         Returns
         -------
         numpy.ndarray
-            Array of computation times, in seconds, of the DIN 45681 parameters over time (decisive
-            difference, uncertainty, and tonal adjustment).
+            Array of the computation times, in seconds, of the DIN 45681 parameters over time
+            (decisive difference, uncertainty, and tonal adjustment).
         """
         return self.get_output_as_nparray()[7]
 
