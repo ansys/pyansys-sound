b4c98101d0f7c86033e702894c614e8bbfdb4d2d
Antoine Minard <antoine.minard@ansys.com>
2024-12-16 10:20:32 +0100

    Update conftest following merge

--- tests/conftest.py
+++ tests/conftest.py
@@ -88,14 +88,18 @@ def pytest_configure():
         os.path.join(base_dir, "AnsysSound_Spectrum_v3_-_nominal_-_dBSPLperHz_2024R2_20241121.txt"),
         server=server,
     )
-    pytest.data_path_filter_frf = "C:\\data\\AnsysSound_FRF_2024R2_20241206.txt"
-    pytest.data_path_filter_frf_wrong_header = "C:\\data\\AnsysSound_FRF_bad_2024R2_20241206.txt"
-
+    pytest.data_path_filter_frf = upload_file_in_tmp_folder(
+        os.path.join(base_dir, "AnsysSound_FRF_2024R2_20241206.txt"),
+        server=server,
+    )
+    pytest.data_path_filter_frf_wrong_header = upload_file_in_tmp_folder(
+        os.path.join(base_dir, "AnsysSound_FRF_bad_2024R2_20241206.txt"),
+        server=server,
+    )
     pytest.data_path_flute_nonUnitaryCalib_as_txt_in_container = upload_file_in_tmp_folder(
         os.path.join(base_dir, "flute_nonUnitaryCalib_as_text_2024R2_20241125.txt"),
         server=server,
     )
-
     pytest.data_path_rpm_profile_as_wav_in_container = upload_file_in_tmp_folder(
         os.path.join(base_dir, "RPM_profile_2024R2_20241126.wav"), server=server
     )
@@ -105,14 +109,12 @@ def pytest_configure():
     pytest.data_path_sound_composer_bbn_source_in_container = upload_file_in_tmp_folder(
         os.path.join(base_dir, "AnsysSound_BBN dBSPL OCTAVE Constants.txt"), server=server
     )
-
     pytest.data_path_sound_composer_bbn_source_40_values_in_container = upload_file_in_tmp_folder(
         os.path.join(
             base_dir, "AnsysSound_BBN dBSPLperHz NARROWBAND v2_40values_2024R2_20241128.txt"
         ),
         server=server,
     )
-
     pytest.data_path_sound_composer_bbn_source_2p_in_container = upload_file_in_tmp_folder(
         os.path.join(
             base_dir, "AnsysSound_BBN_MultipleParameters Pa2PerHz Narrowband v2_2024R2_20240418.txt"
