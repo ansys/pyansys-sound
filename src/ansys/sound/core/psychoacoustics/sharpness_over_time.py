38c28e2bec89ccee02987d12e17bc113c78d665b
Antoine Minard <antoine.minard@ansys.com>
2025-01-23 17:22:00 +0100

    Fixed signal check

--- src/ansys/sound/core/psychoacoustics/sharpness_over_time.py
+++ src/ansys/sound/core/psychoacoustics/sharpness_over_time.py
@@ -74,7 +74,7 @@ class SharpnessOverTime(PsychoacousticsParent):
     @signal.setter
     def signal(self, signal: Field):
         """Set the signal."""
-        if not isinstance(signal, (Field, None)):
+        if not (isinstance(signal, Field) or signal is None):
             raise PyAnsysSoundException("Signal must be specified as a DPF field.")
         self.__signal = signal
 
