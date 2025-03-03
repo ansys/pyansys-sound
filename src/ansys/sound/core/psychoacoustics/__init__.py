bf4428f4fc346dfdcf8f2e6526e9318e18f54bf6
Antoine Minard <165933065+ansaminard@users.noreply.github.com>
2025-03-03 10:17:35 +0100

    feat: Tonality IS/TS 20065 (#239)

--- src/ansys/sound/core/psychoacoustics/__init__.py
+++ src/ansys/sound/core/psychoacoustics/__init__.py
@@ -43,6 +43,7 @@ from .tonality_din_45681 import TonalityDIN45681
 from .tonality_ecma_418_2 import TonalityECMA418_2
 from .tonality_iso_1996_2 import TonalityISO1996_2
 from .tonality_iso_1996_2_over_time import TonalityISO1996_2_OverTime
+from .tonality_iso_ts_20065 import TonalityISOTS20065
 from .tone_to_noise_ratio import ToneToNoiseRatio
 from .tone_to_noise_ratio_for_orders_over_time import ToneToNoiseRatioForOrdersOverTime
 
@@ -60,6 +61,7 @@ __all__ = (
     "Roughness",
     "FluctuationStrength",
     "TonalityDIN45681",
+    "TonalityISOTS20065",
     "TonalityISO1996_2_OverTime",
     "TonalityAures",
     "SpectralCentroid",
