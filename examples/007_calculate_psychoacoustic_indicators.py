9d942b493754d84ae3ae50c4d1315fafa2350bb7
Antoine Minard <antoine.minard@ansys.com>
2025-03-21 18:31:45 +0100

    Fixed some comments and displayed outputs

--- examples/007_calculate_psychoacoustic_indicators.py
+++ examples/007_calculate_psychoacoustic_indicators.py
@@ -30,17 +30,16 @@ This example shows how to calculate psychoacoustic indicators.
 The following indicators are included:
 
 - Loudness of stationary sounds according to ISO 532-1.
-- Loudness over time, according to ISO 532-1.
-- Loudness (monaural ad binaural) of stationary sounds according to ISO 532-2.
+- Loudness of time-varying sounds, according to ISO 532-1.
+- Monaural and binaural loudness of stationary sounds according to ISO 532-2.
 - Sharpness according to Zwicker and Fastl, "Psychoacoustics: Facts and models", 1990.
-- Sharpness over time, according to Zwicker and Fastl.
-- Sharpness according to DIN45692.
-- Sharpness over time, according to DIN45692.
-- Roughness according to Daniel and Weber, "Psychoacoustical Roughness: Implementation of an
-  Optimized Model, 1997.
-- Roughness over time, according to Daniel and Weber.
+- Sharpness according to Zwicker and Fastl, over time.
+- Sharpness according to DIN 45692.
+- Sharpness according to DIN 45692, over time.
+- Roughness and roughness over time according to Daniel and Weber, "Psychoacoustical Roughness:
+  Implementation of an Optimized Model, 1997.
 - Fluctuation strength according to Sontacchi, "Entwicklung eines Modulkonzeptes für die
-  psychoakustische Geräuschanalyse under MatLab Diplomarbeit", 1998.
+  psychoakustische Geräuschanalyse under MatLab", 1998.
 
 The example shows how to perform these operations:
 
@@ -54,12 +53,12 @@ The example shows how to perform these operations:
 # %%
 # Set up analysis
 # ~~~~~~~~~~~~~~~
-# Setting up the analysis consists of loading Ansys libraries, connecting to the
-# DPF server, and retrieving the example files.
+# Setting up the analysis consists of loading libraries, and connecting to the DPF server.
 
-# Load Ansys libraries.
+# Load standard libraries.
 import os
 
+# Load Ansys libraries.
 from ansys.sound.core.examples_helpers import (
     download_accel_with_rpm_wav,
     download_flute_wav,
@@ -87,23 +86,23 @@ my_server = connect_to_or_start_server(use_license_context=True)
 # %%
 # Load the WAV files used in this example
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Load the signals from WAV files using the :class:`.LoadWav` class. It is returned as a
-# :class:`FieldsContainer <ansys.dpf.core.fields_container.FieldsContainer>` object.
+# Load the signals from WAV files using the :class:`.LoadWav` class. They are returned as
+# :class:`FieldsContainer <ansys.dpf.core.fields_container.FieldsContainer>` objects.
 
-# Load example data from WAV file: recording of a flute
+# Load a first signal from a WAV file: recording of a flute.
 path_flute_wav = download_flute_wav(server=my_server)
 wav_loader = LoadWav(path_flute_wav)
 wav_loader.process()
 signal_flute = wav_loader.get_output()[0]
 
-# Load another signal from a WAV file: recording of an acceleration inside a car cabin
+# Load another signal from a WAV file: recording of an acceleration inside a car cabin.
 path_accel_wav = download_accel_with_rpm_wav(server=my_server)
 wav_loader = LoadWav(path_accel_wav)
 wav_loader.process()
 signal_car_acceleration = wav_loader.get_output()[0]
-# The second field of this signal (RPM profile) is useless in this example.
+# Note: This signal contains a second field (RPM profile) that is useless in this example.
 
-# get file names, to use later on
+# Get file names, for later use.
 file_name_flute = os.path.basename(path_flute_wav)
 file_name_car_acceleration = os.path.basename(path_accel_wav)
 
@@ -114,12 +113,12 @@ file_name_car_acceleration = os.path.basename(path_accel_wav)
 
 # %%
 # Create a :class:`.LoudnessISO532_1_Stationary` object, set its signal, and compute the loudness
-# according to standard ISO532-1.
+# according to standard ISO 532-1.
 loudness_ISO532_1_stationary = LoudnessISO532_1_Stationary(signal=signal_flute)
 loudness_ISO532_1_stationary.process()
 
 # %%
-# Get the ISO532-1 loudness (in sone) and loudness level (in phon).
+# Get the ISO 532-1 loudness (in sone) and loudness level (in phon).
 loudness_ISO532_1_sone = loudness_ISO532_1_stationary.get_loudness_sone()
 loudness_ISO532_1_level_phon = loudness_ISO532_1_stationary.get_loudness_level_phon()
 
@@ -130,11 +129,11 @@ print(
 )
 
 # %%
-# Plot the ISO532-1 specific loudness, that is, the loudness at each Bark band index.
+# Plot the ISO 532-1 specific loudness, that is, the loudness at each Bark band index.
 loudness_ISO532_1_stationary.plot()
 
 # %%
-# Calculate ISO 532-1 loudness for a non-stationary sound
+# Calculate ISO 532-1 loudness for a time-varying sound
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # %%
@@ -159,7 +158,7 @@ print(
 )
 
 # %%
-# Plot loudness ISO 532-1 as a function of time.
+# Plot the ISO 532-1 loudness as a function of time.
 loudness_ISO532_1_time_varying.plot()
 
 # %%
@@ -168,28 +167,28 @@ loudness_ISO532_1_time_varying.plot()
 
 # %%
 # Create a :class:`.LoudnessISO532_2` object, set its signal, and compute the loudness
-# according to standard ISO532-2.
+# according to standard ISO 532-2.
 loudness_ISO532_2 = LoudnessISO532_2(signal=signal_flute, field_type="Free")
 loudness_ISO532_2.process()
 
 # %%
-# Get the binaural and monaural ISO532-2 loudness (in sone) and loudness level (in phon).
+# Get the ISO 532-2 binaural and monaural loudness (in sone), and loudness level (in phon).
 loudness_ISO532_2_sone_monaural = loudness_ISO532_2.get_monaural_loudness_sone()[0]
 loudness_ISO532_2_phon_monaural = loudness_ISO532_2.get_monaural_loudness_level_phon()[0]
 loudness_ISO532_2_sone_binaural = loudness_ISO532_2.get_binaural_loudness_sone()
 loudness_ISO532_2_phon_binaural = loudness_ISO532_2.get_binaural_loudness_level_phon()
 
 print(
-    f"\nThe ISO 532-2 loudness of sound file {file_name_flute} is: \n"
-    f" monaural loudness: {loudness_ISO532_2_sone_monaural:.1f} sones, "
+    f"\nThe ISO 532-2 loudness of sound file {file_name_flute} is:\n"
+    f"\tMonaural loudness and loudness level: {loudness_ISO532_2_sone_monaural:.1f} sones, "
     f"{loudness_ISO532_2_phon_monaural:.1f} phons.\n"
-    f" binaural loudness: {loudness_ISO532_2_sone_binaural:.1f} sones, "
+    f"\tBinaural loudness and loudness level: {loudness_ISO532_2_sone_binaural:.1f} sones, "
     f"{loudness_ISO532_2_phon_binaural:.1f} phons."
 )
 
 # %%
-# You can compare the loudness values to the ISO 532-1 loudness values,
-# to see the difference between the two standards.
+# For comparison, display the values previously obtained with the ISO 532-1 standard. The two
+# standard rely on similar auditory principles but differ in the way they estimate the loudness.
 print(
     f"\nThe ISO 532-1 loudness of sound file {file_name_flute} is "
     f"{loudness_ISO532_1_sone:.1f} sones "
@@ -198,7 +197,7 @@ print(
 
 # %%
 # Plot the ISO 532-2 binaural specific loudness, that is,
-# the binaural loudness at each Bark band index.
+# the binaural loudness at each center frequency of equivalent rectangular bandwidth (ERB).
 loudness_ISO532_2.plot()
 
 
@@ -219,24 +218,26 @@ sharpness_over_time.process()
 sharpness_over_time.plot()
 
 # %%
-# Calculate the sharpness DIN45692.
+# Calculate the sharpness according to standard DIN 45692.
 sharpness_DIN45692 = SharpnessDIN45692(signal=signal_flute, field_type="Free")
 sharpness_DIN45692.process()
 sharpness_DIN45692_value = sharpness_DIN45692.get_sharpness()
 
 # %%
-# Calculate the sharpness DIN45692 over time and plot it.
+# Calculate the sharpness according to standard DIN 45692, over time, and plot it.
 sharpness_DIN45692_over_time = SharpnessDIN45692OverTime(signal=signal_flute, field_type="Free")
 sharpness_DIN45692_over_time.process()
 sharpness_DIN45692_over_time.plot()
 
 # %%
-# Calculate the roughness and roughness over time, plot the second one.
+# Calculate the roughness, and plot the specific roughness and roughness over time.
+
+# Calculate the specific roughness, the roughness over time, and the overall roughness.
 roughness = Roughness(signal=signal_flute)
 roughness.process()
 roughness_value = roughness.get_roughness()
 
-# get roughness over time, and plot it.
+# Plot the specific roughness and the roughness over time.
 roughness_over_time = roughness.get_roughness_over_time()
 roughness.plot()
 
@@ -247,11 +248,11 @@ fluctuation_strength.process()
 fluctuation_strength_values = fluctuation_strength.get_fluctuation_strength()
 
 # %%
-# Print the results.
+# Print the calculated indicators.
 print(
     f"\nThe sharpness of sound file {file_name_flute} "
     f"is {sharpness_value:.2f} acum,\n"
-    f"its sharpness DIN45692 is {sharpness_DIN45692_value:.2f} acum,\n"
+    f"its sharpness according to DIN 45692 is {sharpness_DIN45692_value:.2f} acum,\n"
     f"its roughness is {roughness_value:.2f} asper,\n"
     f"and its fluctuation strength is {fluctuation_strength_values:.2f} vacil."
 )
