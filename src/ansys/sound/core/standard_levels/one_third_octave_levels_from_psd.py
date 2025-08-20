e944c0d9b6998323bd465916bb7261ed889ebd31
Antoine Minard <antoine.minard@ansys.com>
2025-08-20 18:21:49 +0200

    missing dash in plots

--- src/ansys/sound/core/standard_levels/one_third_octave_levels_from_psd.py
+++ src/ansys/sound/core/standard_levels/one_third_octave_levels_from_psd.py
@@ -53,7 +53,7 @@ class OneThirdOctaveLevelsFromPSD(FractionalOctaveLevelsFromPSDParent):
 
         if len(self.frequency_weighting) > 0:
             ylabel = (
-                f"{self.frequency_weighting}-weighted 1/3octave-band level "
+                f"{self.frequency_weighting}-weighted 1/3-octave-band level "
                 f"(dB{self.frequency_weighting} re {self.reference_value})"
             )
         else:
