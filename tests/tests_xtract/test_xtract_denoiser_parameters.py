56e46a3bfd8e3485b73b6d93b44ea7d00a709810
Antoine Minard <antoine.minard@ansys.com>
2025-10-29 08:46:11 +0100

    Revert "keep tests up to signal utilities"
    This reverts commit a55b5c4495452a9f2b2315b5d17044b50984a105.

---
+++ tests/tests_xtract/test_xtract_denoiser_parameters.py
@@ -0,0 +1,102 @@
+# Copyright (C) 2023 - 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from ansys.dpf.core import Field
+import numpy as np
+import pytest
+
+from ansys.sound.core._pyansys_sound import PyAnsysSoundException
+from ansys.sound.core.signal_utilities import LoadWav
+from ansys.sound.core.xtract.xtract_denoiser_parameters import XtractDenoiserParameters
+
+
+def test_xtract_denoiser_parameters_instantiation():
+    xtract_denoiser_parameters = XtractDenoiserParameters()
+    assert xtract_denoiser_parameters != None
+
+
+def test_xtract_denoiser_parameters_getter_setter_upper_threshold():
+    xtract_denoiser_parameters = XtractDenoiserParameters()
+
+    # Invalid value
+    with pytest.raises(PyAnsysSoundException) as excinfo:
+        xtract_denoiser_parameters.noise_psd = None
+    assert str(excinfo.value) == "Noise PSD must be a non-empty field."
+
+    xtract_denoiser_parameters.noise_psd = Field()
+
+    assert type(xtract_denoiser_parameters.noise_psd) == Field
+
+
+def test_xtract_denoiser_parameters_getter_generic_data_container():
+    xtract_denoiser_parameters = XtractDenoiserParameters()
+
+    gdc = xtract_denoiser_parameters.get_parameters_as_generic_data_container()
+    assert gdc is not None
+
+
+def test_xtract_denoiser_parameters_generate_noise_psd_from_white_noise_level():
+    xtract_denoiser_parameters = XtractDenoiserParameters()
+
+    noise_psd = xtract_denoiser_parameters.create_noise_psd_from_white_noise_level(
+        white_noise_level=(40.0 + 94.0), sampling_frequency=44100.0, window_length=50
+    )
+
+    noise = noise_psd.data
+
+    s = np.sum(noise)
+    assert s == pytest.approx(464.853)
+
+
+def test_xtract_denoiser_parameters_generate_noise_psd_from_automatic_estimation():
+    wav_loader = LoadWav(pytest.data_path_white_noise_in_container)
+    wav_loader.process()
+    fc_signal = wav_loader.get_output()
+    xtract_denoiser_parameters = XtractDenoiserParameters()
+
+    noise_psd = xtract_denoiser_parameters.create_noise_psd_from_automatic_estimation(fc_signal[0])
+
+    noise = noise_psd.data
+
+    s = np.sum(noise)
+
+    if pytest.SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_11_0:
+        # bug fix in DPF Sound 2026 R1 ID#1247009
+        assert s == pytest.approx(0.00522007046561157)
+    else:  # DPF Sound <= 2025 R2
+        assert s == pytest.approx(0.0051941522299330245)
+
+
+def test_xtract_denoiser_parameters_generate_noise_psd_from_noise_samples():
+    wav_loader = LoadWav(pytest.data_path_white_noise_in_container)
+    wav_loader.process()
+    fc_signal = wav_loader.get_output()
+    xtract_denoiser_parameters = XtractDenoiserParameters()
+
+    noise_psd = xtract_denoiser_parameters.create_noise_psd_from_noise_samples(
+        fc_signal[0], 44100.0
+    )
+
+    noise = noise_psd.data
+
+    s = np.sum(noise)
+    assert s == pytest.approx(0.0025502318834469406)
---
+++ tests/tests_xtract/test_xtract_transient_parameters.py
@@ -0,0 +1,64 @@
+# Copyright (C) 2023 - 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import pytest
+
+from ansys.sound.core._pyansys_sound import PyAnsysSoundException
+from ansys.sound.core.xtract.xtract_transient_parameters import XtractTransientParameters
+
+
+def test_xtract_transient_parameters_instantiation():
+    xtract_transient_parameters = XtractTransientParameters()
+    assert xtract_transient_parameters != None
+
+
+def test_xtract_transient_parameters_getter_setter_upper_threshold():
+    xtract_transient_parameters = XtractTransientParameters()
+
+    # Invalid value
+    with pytest.raises(PyAnsysSoundException) as excinfo:
+        xtract_transient_parameters.upper_threshold = 150.0
+    assert str(excinfo.value) == "Upper threshold must be between 0.0 and 100.0 dB."
+
+    xtract_transient_parameters.upper_threshold = 92.0
+
+    assert xtract_transient_parameters.upper_threshold == 92.0
+
+
+def test_xtract_transient_parameters_getter_setter_lower_threshold():
+    xtract_transient_parameters = XtractTransientParameters()
+
+    # Invalid value
+    with pytest.raises(PyAnsysSoundException) as excinfo:
+        xtract_transient_parameters.lower_threshold = 150.0
+    assert str(excinfo.value) == "Lower threshold must be between 0.0 and 100.0 dB."
+
+    xtract_transient_parameters.lower_threshold = 92.0
+
+    assert xtract_transient_parameters.lower_threshold == 92.0
+
+
+def test_xtract_transient_parameters_getter_generic_data_container():
+    xtract_transient_parameters = XtractTransientParameters()
+
+    gdc = xtract_transient_parameters.get_parameters_as_generic_data_container()
+    assert gdc is not None
