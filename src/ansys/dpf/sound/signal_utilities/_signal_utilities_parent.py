379c6e595a9a2f5ad81ee9c08d1b49d01a4379ed
a-bouth <a-bouth@users.noreply.github.com>
2026-01-11 02:23:46 +0000

    DOC: update development documentation 8a5cbc90bc600b7656087339a48a859245e6b644

---
+++ src/ansys/dpf/sound/signal_utilities/_signal_utilities_parent.py
@@ -0,0 +1,48 @@
+"""Signal Utilities."""
+from ansys.dpf.core import Field
+import matplotlib.pyplot as plt
+
+from ..pydpf_sound import PyDpfSound
+
+
+class SignalUtilitiesParent(PyDpfSound):
+    """
+    Abstract mother class for signal utilities.
+
+    This is the mother class of all signal utilities classes, should not be used as is.
+    """
+
+    def __init__(self):
+        """Init.
+
+        Init the class.
+        """
+        super().__init__()
+
+    def plot(self):
+        """Plot signals.
+
+        Plots the resulting signals in a single figure.
+        """
+        output = self.get_output()
+
+        if type(output) == Field:
+            num_channels = 0
+            field = output
+        else:
+            num_channels = len(output)
+            field = output[0]
+
+        time_data = field.time_freq_support.time_frequencies.data
+        time_unit = field.time_freq_support.time_frequencies.unit
+        unit = field.unit
+
+        for i in range(num_channels):
+            plt.plot(time_data, output[i].data, label="Channel {}".format(i))
+
+        plt.title(field.name)
+        plt.legend()
+        plt.xlabel(time_unit)
+        plt.ylabel(unit)
+        plt.grid(True)
+        plt.show()
