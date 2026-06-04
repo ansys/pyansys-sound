695c82e95224ac73a79e29b949e26a5be06129fe
Antoine Minard <165933065+ansaminard@users.noreply.github.com>
2026-06-04 14:40:12 +0200

    Apply suggestion from @ansaminard

--- tests/tests_standard_levels/test_standard_levels_level_over_time.py
+++ tests/tests_standard_levels/test_standard_levels_level_over_time.py
@@ -53,7 +53,7 @@ elif pytest.SOUND_VERSION_GREATER_THAN_OR_EQUAL_TO_2026R1:
         'LevelOverTime object.\nData\n\tSignal: "flute"\n\tScale type: dB\n'
         "\tReference value: 2e-05\n\tFrequency weighting: None\n\tTime weighting: Custom\n"
         "\tTime step: 100.0 ms\n\tWindow size: 5000.0 ms\n\tAnalysis window: HANN\n"
-        "Maximum level: 89.2 dB re. 2e-05 Pa"
+        "Maximum level: 89.2 dB (re. 2e-05 Pa)"
     )
 else:
     EXP_STR_ALL_PROCESSED = (
